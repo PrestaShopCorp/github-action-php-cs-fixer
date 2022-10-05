c5e02b40669d89e01e2cbc7441efbe8d308e56f1
john.R <jonathan_renard@hotmail.fr>
2022-10-05 16:44:12 +0200

    feat: add working directory to action parameters

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,12 @@
 #!/bin/sh -l
+
+if [ ! -z "$1" ]
+then
+  cd $1
+
+  shift
+    set -- "$*"
+fi
+
 composer global require prestashop/php-dev-tools:^v3.14
 php ~/.composer/vendor/bin/php-cs-fixer fix $*
