ebfeec1982222eb8d41d257871119042df1c61b3
john.R <jonathan_renard@hotmail.fr>
2022-10-05 16:54:38 +0200

    fix: second param

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,7 @@
 if [ ! -z "$1" ]
 then
   cd $1
-
-  shift
-    set -- "$*"
 fi
 
 composer global require prestashop/php-dev-tools:^v3.14
-php ~/.composer/vendor/bin/php-cs-fixer fix $*
+php ~/.composer/vendor/bin/php-cs-fixer fix $2
